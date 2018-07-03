e7ee9baca27afb240a90d891d23c9491142066a0
Dmytro <dima.smolts@gmail.com>
2018-07-03 17:10:42 +0300

    Simple menu created

--- Project/Program.cs
+++ Project/Program.cs
@@ -10,6 +10,47 @@ namespace Project
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Hello Binary Studio Academy 2018!");
+			while (true)
+			{
+				Console.WriteLine("\n*******************************************");
+				Console.WriteLine("  choose the task (enter only one number)");
+				Console.WriteLine("  1 ");
+				Console.WriteLine("  2 ");
+				Console.WriteLine("  3 ");
+				Console.WriteLine("  4 ");
+				Console.WriteLine("  5 ");
+				Console.WriteLine("  6 ");
+				Console.WriteLine("  7 - Update");
+				Console.WriteLine("  8 - Exit");
+				Console.WriteLine("*******************************************");
+				Console.Write("===> ");
+				string input = Console.ReadLine();
+				Console.WriteLine("*******************************************");
+				switch (input)
+				{
+					case "1":						
+						break;
+					case "2":
+						break;
+					case "3":
+						break;
+					case "4":
+						break;
+					case "5":
+						break;
+					case "6":
+						break;
+					case "7":
+						break;
+					case "8":
+						Environment.Exit(0);
+						break;
+					default:
+						Console.WriteLine("wrong input");
+						break;
+				}
+			}
 		}
 	}
 }
